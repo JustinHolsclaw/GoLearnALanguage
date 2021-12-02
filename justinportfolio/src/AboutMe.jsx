67dba444cd39ac598ce77da62d820b63c18236e7
Tacocatman12 <jholsclaw@live.com>
2021-12-02 11:40:16 -0700

    needed to merge

--- justinportfolio/src/AboutMe.jsx
+++ justinportfolio/src/AboutMe.jsx
@@ -1,15 +1,14 @@
 import JustinHolsclaw from "./Images/JustinHolsclaw.png"
 
 function AboutMe() {
-  const headerText = "Justin Holsclaw";
   const descriptionText =
     "A Software Engineer with both experience and a degree in Software Engineering.";
   return (
     <div className="row">
       <div className="col justinimage mr-10">
-        <img className ="img-fluid shadow-lg" src={JustinHolsclaw}></img>
+        <img className ="img-fluid shadow-lg" src={JustinHolsclaw} alt=""></img>
       </div>
-      <div className="col border border-4 border-dark rounded shadow-lg">
+      <div className="col shadow-lg">
         <p className="h1 text-center align-middle"><strong>{descriptionText}</strong></p>
       </div>
     </div>
--- justinportfolio/src/NavBar.jsx
+++ justinportfolio/src/NavBar.jsx
@@ -1,6 +1,8 @@
+import "./custom.scss"
+
 function NavBar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <strong>Justin Holsclaw</strong>
