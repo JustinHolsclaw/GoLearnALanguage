b527435b1c0fcf1e02d23fd1ad875197ecb02856
Tacocatman12 <jholsclaw@live.com>
2021-12-04 19:29:54 -0700

    Made the switch

--- justinportfolio/src/MainSwitch.js
+++ justinportfolio/src/MainSwitch.js
@@ -1,11 +1,12 @@
 import {BrowserRouter as Router, Route, Routes, useRoutes } from "react-router-dom";
 import Home from "./Home";
+import Project from "./Project";
 const MainSwitch = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/projects" />
+        <Route path="/projects" element={<Project/>}/>
         <Route path="/resume" />
       </Routes>
     </Router>
---
+++ justinportfolio/src/Views/Project.jsx
@@ -0,0 +1,8 @@
+const Project = () =>{
+    
+    return(
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+}
